cd7ce54d5dfc8831e4f6ec404af225cc7e42bd22
Sander <sandervdb23@gmail.com>
2025-04-12 12:11:52 +0700

    add cors and credentials to produk api

--- backend/admin/pemesanan/update.php
+++ backend/admin/pemesanan/update.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:5500"); // sesuaikan dengan asal frontend
+header("Access-Control-Allow-Origin: http://localhost:5500");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: PUT, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
--- backend/admin/produk/update.php
+++ backend/admin/produk/update.php
@@ -1,4 +1,15 @@
 <?php
+header("Access-Control-Allow-Origin: http://localhost:5500");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: PUT, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json; charset=UTF-8");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 require_once '../auth/verify.php';
 require_once '../../config/database.php';
 
